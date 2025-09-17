a476de3dc82a85fe8c15a1531ff3637c76885edd
james <B10bot@protonmail.com>
2025-09-16 21:47:50 -0500

    execution

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/jamesh000/fsm-sim/stack"
 )
 
 type state struct {
@@ -27,7 +29,7 @@ func main() {
 	}
 
 	fmtDefFile := args[0]
-	//str := args[1]
+	str := args[1]
 
 	fmtDefString, err := os.ReadFile(fmtDefFile)
 
@@ -44,8 +46,9 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Parsed successfully, startstate with arrays", *startState, startState.transitions[0][0], startState.transitions[1][0])
+	//fmt.Println("Parsed successfully, startstate with arrays", *startState, startState.transitions[0][0], startState.transitions[1][0])
 
+	execute(startState, str)
 }
 
 func tokenize(strIn string) [][]string {
@@ -120,10 +123,48 @@ func parse(tokens [][]string) (*state, error) {
 	return startState, nil
 }
 
-/*
 func execute(initialState *state, input string) {
-	exeStack := Stack[partialExe]{}
+	exeStack := stack.Stack[partialExe]{}
 
+	// initial execution
+	exeStack.Push(partialExe{initialState, input})
 
+	for !exeStack.IsEmpty() {
+		// get next execution
+		e := exeStack.Pop()
+
+		fmt.Println("Moving to state", e.s.name)
+
+		if len(e.remainingInput) == 0 {
+			fmt.Println("End of input reached on state", e.s.name)
+			if e.s.final {
+				fmt.Println("The input is valid!")
+				return
+			} else {
+				fmt.Println("Path is dead")
+				fmt.Println()
+			}
+
+			// go to the next thing
+			continue
+		}
+
+		for _, s := range e.s.epsilonTransitions {
+			exeStack.Push(partialExe{s, e.remainingInput})
+		}
+
+		c := e.remainingInput[0]
+		switch c {
+		case '0':
+			for _, s := range e.s.transitions[0] {
+				exeStack.Push(partialExe{s, e.remainingInput[1:]})
+			}
+		case '1':
+			for _, s := range e.s.transitions[1] {
+				exeStack.Push(partialExe{s, e.remainingInput[1:]})
+			}
+		}
+	}
+
+	fmt.Printf("No more paths to take...")
 }
-*/
